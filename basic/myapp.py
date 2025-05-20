e9fa1d3eb4fad49e7317796c08495f08a397b044
jacky <jackyfan@163.com>
2025-05-20 18:18:56 +0800

    优化

--- basic/myapp.py
+++ basic/myapp.py
@@ -7,9 +7,9 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("主界面")
-        bt = QPushButton("按我一下")
+        btn = QPushButton("按我一下")
         self.setFixedSize(QSize(300, 400))
-        self.setCentralWidget(bt)
+        self.setCentralWidget(btn)
 
 app = QApplication(sys.argv)
 window = MainWindow()
